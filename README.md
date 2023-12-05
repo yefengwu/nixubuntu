f978ea976f605844ec30cf999a8d5b159c5674a1
xuwei <franklxw@outlook.com>
2023-12-05 14:42:12 +0800

    Nix system manage for ubuntu

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nixubuntu
 Nix System manage for Ubuntu 
+
+## Usage
+
+```bash
+sudo nix run 'github:numtide/system-manager' -- switch --flake '$HOME/.config/nix'
+```
---
+++ modules/default.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+{
+  config = {
+    nixpkgs.hostPlatform = "x86_64-linux";
+
+    environment = {
+      etc = {
+        "foo.conf".text = ''
+          launch_the_rockets = true
+        '';
+      };
+      systemPackages = [
+	pkgs.chezmoi
+        pkgs.ripgrep
+        pkgs.fd
+	pkgs.zellij
+        pkgs.hello
+	pkgs.gh
+	pkgs.neovim
+	pkgs.joshuto
+	pkgs.docker
+      ];
+    };
+
+    systemd.services = {
+      foo = {
+        enable = true;
+        serviceConfig = {
+          Type = "oneshot";
+          RemainAfterExit = true;
+        };
+        wantedBy = [ "system-manager.target" ];
+        script = ''
+          ${lib.getBin pkgs.hello}/bin/hello
+          echo "We launched the rockets!"
+        '';
+      };
+    };
+  };
+}
